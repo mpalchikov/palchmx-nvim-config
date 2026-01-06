f0d1fc59650b9305ce71f91acc43d68380bc0c11
Maksym Palchykov <maksym.palchykov@uklon.com.ua>
2026-01-06 12:35:04 +0200

    Add json and yaml lsp

--- lua/settings.lua
+++ lua/settings.lua
@@ -62,3 +62,42 @@ vim.lsp.enable("luals")
 
 vim.lsp.config["roslyn"] = {}
 vim.lsp.enable("roslyn")
+
+vim.lsp.config['jsonls'] = {
+  cmd = { 'vscode-json-language-server', '--stdio' },
+  filetypes = { 'json', 'jsonc' },
+  root_markers = { 'package.json', '.git' },
+  -- Required for completions to work properly in jsonls
+  capabilities = {
+    textDocument = {
+      completion = {
+        completionItem = {
+          snippetSupport = true,
+        },
+      },
+    },
+  },
+  settings = {
+    json = {
+      validate = { enable = true },
+    },
+  },
+}
+vim.lsp.enable('jsonls')
+
+vim.lsp.config['yamlls'] = {
+  cmd = { 'yaml-language-server', '--stdio' },
+  filetypes = { 'yaml', 'yaml.docker-compose', 'yaml.gitlab' },
+  root_markers = { '.git' },
+  settings = {
+    yaml = {
+      schemaStore = {
+        enable = true,
+        url = "https://www.schemastore.org/api/json/catalog.json",
+      },
+      format = { enable = true },
+      validate = true,
+    },
+  },
+}
+vim.lsp.enable('yamlls')
