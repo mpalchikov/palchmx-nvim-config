e7f03e82ff84cda06d0bf9585b3f088de06f93b0
Maksym Palchykov <maksym.palchykov@uklon.com.ua>
2026-02-13 08:38:49 +0200

    Change keymaps

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -74,8 +74,8 @@ return {
         vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
         vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
         vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
-        vim.keymap.set("n", "<leader>gr", builtin.lsp_references, { desc = "Telescope LSP references" })
-        vim.keymap.set("n", "<leader>xd", builtin.diagnostics, { desc = "Telescope Diagnostics"})
+        vim.keymap.set("n", "<leader>fr", builtin.lsp_references, { desc = "Telescope LSP references" })
+        vim.keymap.set("n", "<leader>fd", builtin.diagnostics, { desc = "Telescope Diagnostics"})
         vim.keymap.set("n", "<leader>fu", builtin.git_status, { desc = "Telescope git status (uncommitted files)" })
         vim.keymap.set("n", "<leader>ss", builtin.lsp_document_symbols, { desc = "[S]earch [S]ymbols (Document)" })
 
